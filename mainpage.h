7abbb5e062c81dcea536724a3a7a95bd6edfd7b5
useger <useger@romandie.com>
2013-07-03 23:33:31 +0200

    add: mainpage, .gitignore

---
+++ mainpage.h
@@ -0,0 +1,24 @@
+//==================================================================================================
+//                                              HFU
+//==================================================================================================
+//
+//! (c) Copyright 2013 HFU Hoehere Fachschule Uster, Switzerland. All Rights Reserved.
+//! \author		Urban Seger, Konolfingen
+//
+// $Date: $:
+// $State: $:
+// $Revision: $:
+//
+//==================================================================================================
+
+/**
+\mainpageSTM_EVAL_LEDToggle(LED3);
+This is final project (duration: 1 week) terminating the MCU course by Matthias Klaey at HF Uster. The aim of this project was to programm a simplistic <b>logic analyser</b> based on an ARM microcontrollers. More properly speaking, the programmed device would rather be a multi channel digital <b>data logger</b> that collects data, parsed it into an adequate format and sends it via a serial connection to a PC for offline analysis.
+
+Links:
+\htmlonly
+<ul>
+<a href=http://www.hfu.ch>HFU Webpage</a><br>
+</ul>
+\endhtmlonly
+*/
